f8cd4898e6b59a64c5329fac92b8b7834d5e6e66
angadrazz <angadraj9534@gmail.com>
2026-05-09 09:06:10 +0530

    check mailar

---
+++ check_mailer.php
@@ -0,0 +1,4 @@
+<?php
+require_once "mail_config.php";
+echo "PHPMailer Loaded Successfully!";
+?>
--- mail_config.php
+++ mail_config.php
@@ -1,36 +1,39 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+// Correct absolute path load
+// require_once __DIR__ . "/phpmailer/src/Exception.php";
+// require_once __DIR__ . "/phpmailer/src/PHPMailer.php";
+// require_once __DIR__ . "/phpmailer/src/SMTP.php";
 
 function sendMail($to, $subject, $message){
 
-  $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
-  try {
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.hostinger.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'info@koshiinstitute.org';
-    $mail->Password   = 'Admin@123';  // <-- change
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = 587;
+    try {
 
-    $mail->setFrom('info@koshiinstitute.org', 'Koshi Institute');
-    $mail->addAddress($to);
+        $mail->isSMTP();
+        $mail->Host       = "smtp.hostinger.com";
+        $mail->SMTPAuth   = true;
+        $mail->Username   = "info@koshiinstitute.org";
+        $mail->Password   = "Admin@123";  // Replace with real email password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
 
-    $mail->isHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body    = $message;
+        $mail->setFrom("info@koshiinstitute.org", "Koshi Institute");
+        $mail->addAddress($to);
 
-    $mail->send();
-    return true;
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
 
-  } catch (Exception $e) {
-    return false;
-  }
+        $mail->send();
+        return true;
+
+    } catch (Exception $e) {
+        return false;
+    }
 }
 ?>
